58d005a13dfeebb8e79496db8539ff18659d19b5
Egor@Egora <baidashevegor@yandex.ru>
2020-07-13 23:44:57 +0500

    Создан DOM-элемент объявления (карточка объявления), заполнен данными из объекта

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ var PIN_Y_MAX = 630;
 var MIN_PRICE = 0;
 var MAX_PRICE = 1000000;
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var ROOMS = [1, 2, 3];
-var GUESTS = [1, 2, 3];
+var ROOMS = [1, 2, 3, 4, 5];
+var GUESTS = [1, 2, 3, 4, 5, 6, 7, 8];
 var CHECKIN = ['12:00', '13:00', '14:00'];
 var CHECKOUT = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -96,3 +96,58 @@ renderPins();
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
+
+var typesOffers = {
+  palace: {
+    ru: 'Дворец'
+  },
+  flat: {
+    ru: 'Квартира'
+  },
+  house: {
+    ru: 'Дом'
+  },
+  bungalo: {
+    ru: 'Бунгало'
+  }
+};
+
+var filtersContainer = document.querySelector('.map__filters-container');
+var cardTemplate = document.querySelector('#card').content.querySelector('.map__card');
+
+var getNoun = function (number, one, two, many) {
+  number = Math.abs(number);
+  number %= 100;
+  if (number >= 5 && number <= 20) {
+    return number + many;
+  }
+  number %= 10;
+  if (number === 1) {
+    return number + one;
+  }
+  if (number >= 2 && number <= 4) {
+    return number + two;
+  }
+  return number + many;
+};
+
+var getCard = function (value) {
+  var cardElement = cardTemplate.cloneNode(true);
+
+  var photoElement = cardElement.querySelector('.popup__photos');
+
+  cardElement.querySelector('.popup__title').textContent = value.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = value.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = value.offer.price + ' ₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = typesOffers[value.offer.type].ru;
+  cardElement.querySelector('.popup__text--capacity').textContent = getNoun(value.offer.rooms, ' комната для ', ' комнаты для ', ' комнат для ') + getNoun(value.offer.guests, ' гостя', ' гостей', ' гостей');
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + value.offer.checkin + ', выезд до ' + value.offer.checkout;
+  cardElement.querySelector('.popup__description').textContent = value.offer.description;
+  cardElement.querySelector('.popup__avatar').setAttribute('src', value.author.avatar);
+  cardElement.querySelectorAll('.popup__features').textContent = value.offer.features;
+  photoElement.querySelector('img').src = value.offer.photos[0];
+
+  return cardElement;
+};
+
+map.insertBefore(getCard(resultData[0]), filtersContainer);
